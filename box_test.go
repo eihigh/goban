e43358bfdbb76439f1a961f5c272ec6e51ec465c
eihigh <eihigh.contact@gmail.com>
2019-08-01 18:00:01 +0900

    feature: add mouse support (experimental)

---
+++ box_test.go
@@ -0,0 +1,26 @@
+package goban
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFit(t *testing.T) {
+	a := NewBox(0, 0, 10, 10)
+	b := NewBox(10, 10, 10, 10)
+
+	tests := []struct {
+		got, want *Box
+	}{
+		{a.Fit(b, -1, -1, -1, -1), NewBox(10, 10, 10, 10)},
+		{a.Fit(b, 0, 0, -1, -1), NewBox(5, 5, 10, 10)},
+		{a.Fit(b, 0, 0, 0, 0), NewBox(10, 10, 10, 10)},
+		{a.Fit(b, -1, -1, 1, 1), NewBox(20, 20, 10, 10)},
+	}
+
+	for _, tt := range tests {
+		if !reflect.DeepEqual(tt.want, tt.got) {
+			t.Errorf("got %v; want %v", tt.got, tt.want)
+		}
+	}
+}
---
+++ examples/click/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+
+	"github.com/eihigh/goban"
+)
+
+var (
+	text = "click here"
+)
+
+func main() {
+	goban.Main(app)
+}
+
+func app(_ context.Context, es goban.Events) error {
+	v := &buttonView{}
+	goban.PushView(v)
+
+	for {
+		goban.Show()
+		if e := es.ReadMouse(); v.button.IsClicked(e) {
+			if text == "on" {
+				text = "off"
+			} else {
+				text = "on"
+			}
+		}
+	}
+}
+
+type buttonView struct {
+	button *goban.Box
+}
+
+func (v *buttonView) View() {
+	v.button = goban.NewBox(0, 0, 15, 3).CenterOf(goban.Screen())
+	v.button.Enclose("button").Puts(text)
+}
